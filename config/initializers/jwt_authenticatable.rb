6ff985296b4d45f549d27c5398b19dce39d3083c
Dalvir Singh <dalvir.singh@cerner.com>
2020-05-30 23:24:17 -0400

    authenticated resource working

--- config/initializers/jwt_authenticatable.rb
+++ config/initializers/jwt_authenticatable.rb
@@ -7,30 +7,31 @@ module Devise
     class JWTAuthenticatable < Base
       # Here check if there is token present
       def authenticate!
-        # byebug
+          byebug
         if user
           success!(user)
         else
           fail!('Invalid email or password')
+        end
+
       end
 
-        def user
-          @user ||= Host.find(decoded_auth_token[:user_id]) if decoded_auth_token
-          @user || errors.add(:token, 'Invalid token') && nil
-        end
+      def user
+        @user ||= Host.find(decoded_auth_token[:user_id]) if decoded_auth_token
+        @user || errors.add(:token, 'Invalid token') && nil
+      end
 
-        def decoded_auth_token
-          @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
-        end
+      def decoded_auth_token
+        @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+      end
 
-        def http_auth_header
-          if params['auth_token'].present?
-            params['auth_token'].split(' ').last
-          else
-            errors.add :token, 'Missing token'
-          end
+      def http_auth_header
+        if params['auth_key'].present?
+          params['auth_key'].split(' ').last
+        else
+          errors.add :token, 'Missing token'
         end
+      end
     end
   end
-end
 end
