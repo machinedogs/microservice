583981c1e89240719c7c06f74d9fb9095ecf4d29
Dalvir Singh <dalvir.singh@cerner.com>
2020-05-31 16:46:37 -0400

    heroku logging

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -14,7 +14,7 @@ class AuthenticateUser
   # Returns a jwt token if the user method  is successful
   def call
     puts "User Here"
-    puts user.id
+    puts JsonWebToken.encode(host_id: user.id) if user
     JsonWebToken.encode(host_id: user.id) if user
   end
 
