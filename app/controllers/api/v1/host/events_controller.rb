140a3398124479e14fcaf801da68c2ef021300a6
Dalvir Singh <dalvir.singh@cerner.com>
2020-06-28 17:43:55 -0400

    update

--- app/controllers/api/v1/host/events_controller.rb
+++ app/controllers/api/v1/host/events_controller.rb
@@ -81,7 +81,7 @@ class Api::V1::Host::EventsController < ApplicationController
     # Get user
     user = AuthorizeApiRequest.call(params).result
     #See if event exists and does not belong to this host
-    if (Event.find(params[:event]) && !user.event.exists?(params[:event]) && user.saved_events.include?(params[:event]))
+    if (Event.find(params[:event]) && !user.event.exists?(params[:event]) && !user.saved_events.include?(params[:event]))
       # save event
       if user.saved_events == nil
         user.update!(saved_events: [ params[:event] ] )
