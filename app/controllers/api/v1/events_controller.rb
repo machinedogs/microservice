c8e37a4eaef1812759f23ec38ae67308c7b5e8ef
Dalvir Singh <dalvir.singh@cerner.com>
2020-07-11 12:06:42 -0400

    Update events_controller.rb

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -7,10 +7,12 @@ class Api::V1::EventsController < ApplicationController
   def index
     #check params 
     @events = Event.all
-    user_location = {latitude: params[:latitude], longitude: params[:longitude]}
-    #Initially, we want to send events closest to them 
-    @events = @events.select do |event|
+    if(params[:latitude] && params[:longitude])
+      user_location = {latitude: params[:latitude], longitude: params[:longitude]}
+      #Initially, we want to send events closest to them 
+      @events = @events.select do |event|
       Geocoder::Calculations.distance_between([user_location[:latitude], user_location[:longitude]], [event.latitude, event.longitude])< 200
+      end
     end
     render :events, status: :ok
   end
